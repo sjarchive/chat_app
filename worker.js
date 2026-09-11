85072a4299aa1d86d154e968d53322c5f5370b03
SJ <116071092+sjarchive@users.noreply.github.com>
2026-09-11 09:10:10 +0530

    Delete files

--- worker.js
+++
@@ -1,7 +0,0 @@
-export default {
-  async fetch(request, env) {
-    // For now, just serve the static PWA files.
-    // The push-notification API route will be added here in the next step.
-    return env.ASSETS.fetch(request);
-  },
-};
